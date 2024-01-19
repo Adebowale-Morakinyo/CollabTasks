c910b293ad99bd8e0dccbc11cd4c5d65d954fd41
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2024-01-19 23:44:12 +0100

    TODOs

--- app/algorithms.py
+++ app/algorithms.py
@@ -1,4 +1,6 @@
+# TODO: Get user preferences
+
 def prioritize_tasks(tasks):
-    # Implement my custom task prioritization algorithm here
-    # For simplicity/Testing, we'll sort tasks based on priority
-    return sorted(tasks, key=lambda x: x.priority, reverse=True)
+    # TODO: Calculate priority scores for tasks
+    
+    return
