538b23942a60c47d77d1a574ba0a41d2af4c7a2a
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2024-01-19 23:45:40 +0100

    TODOS

--- app/algorithms.py
+++ app/algorithms.py
@@ -1,6 +1,10 @@
 # TODO: Get user preferences
 
 def prioritize_tasks(tasks):
+    # TODO: Get user-specific weights
+
     # TODO: Calculate priority scores for tasks
-    
+
+    # TODO: Prioritize tasks based on priority scores
+
     return
